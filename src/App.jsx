f50c669c6d29a643b18d77a4e43cabaa0c56ff36
Kevin Hang <herezy2006@gmail.com>
2023-03-27 13:02:28 +0200

    lets have fun

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+
+  return (
+    <div>
+      Folio3D
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/canvas/index.js
@@ -0,0 +1,6 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import Model3DCanvas from "./Model3D";
+import StarsCanvas from "./Stars";
+
+export { EarthCanvas, BallCanvas, Model3DCanvas, StarsCanvas };
