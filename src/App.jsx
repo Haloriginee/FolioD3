6c4c7d520c0a712a26c8408f3fd31e3cc750428c
Kevin Hang <herezy2006@gmail.com>
2023-04-03 13:42:30 +0200

    structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter } from "react-router-dom";
+import { Hero, About, Navbar, Experience, Feedbacks, Tech, Contact, Works, StarsCanvas } from './components';
+
 const App = () => {
 
   return (
-    <div>
-      Folio3D
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-cover bg-hero-pattern bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
