14b2eb1ff1b8be3678ebf41ccebfcac9f390cfd7
Kevin Hang <herezy2006@gmail.com>
2023-04-04 13:29:19 +0200

    logo, navbar on Pc done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { NavLinks } from '../constants';
+import { style } from '../style';
+
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState(" ");
+
+
   return (
-    <div>Navbar</div>
+    <nav
+      className=
+        {`${style.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full max-w-7xl mx-auto justify-between flex items-center">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive(" ");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="w-12 h-12 object-contain"
+          />
+          <p className='text-white font-bold cursor-pointer text-[18px]'>
+            Hang Kévin
+            <span className='sm:block hidden'>
+              | Phôking Hangsome
+            </span>
+          </p>
+
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {NavLinks.map((Link) => (
+            <li key={Link.id} className={`${
+              active === Link.title
+                ? "text-white"
+                : "text-secondary"
+            }
+              hover:text-white cursor-pointer font-medium text-[18px]
+            `}
+              onClick={() => setActive(Link.title)}
+            >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
