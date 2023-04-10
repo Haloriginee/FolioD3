4e52c3f21f350cb008e41d29e91c05c5f846294a
Kevin Hang <herezy2006@gmail.com>
2023-04-10 20:30:48 +0200

    added loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Html, useProgress } from '@react-three/drei';
 
 const Loader = () => {
+
+  const { progress } = useProgress();
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className='canvas-loader'>
+        <p
+          style={{
+            color: '#fff',
+            fontSize: 14,
+            fontWeight: 600,
+            marginTop: 40
+          }}
+        >
+          {progress.toFixed(2)}%
+        </p>
+      </span>
+    </Html>
   )
 }
 
